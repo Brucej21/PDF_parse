5b745dec590c72e59da70eb9ea93be8db0c56193
Juice21 <brucejenkins21@gmail.com>
2017-01-07 00:21:57 +0000

    this is an update

--- pdfwork.py
+++ pdfwork.py
@@ -1,1 +1,7 @@
 #PDF extraction work
+import PyPDF2
+pdfFileObj = open('TargetPDF.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#pdfReader.numPages
+pageObj = pdfReader.getPage(1)
+pageObj.extractText()
